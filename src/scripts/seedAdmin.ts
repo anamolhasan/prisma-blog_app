41fa8c09d5c9385840bbff9d25e20041e2b1c4ad
anamolhasan <mdanamolhasan1998@gmail.com>
2026-01-06 19:20:18 +0600

    seedAdmin learing

---
+++ src/scripts/seedAdmin.ts
@@ -0,0 +1,53 @@
+import { email } from "better-auth/*"
+import { UserRole } from "../middleware/auth"
+import { prisma } from "../lib/prisma"
+
+
+async function seedAdmin () {
+    try {
+        console.log('***** Admin Seeding started ******')
+        const adminData = {
+            name: 'Admin2 Saheb',
+            email:'admin2@admin.com',
+            role:UserRole.ADMIN,
+            password:'admin1234'
+        }
+        console.log('*** Checking Admin Exist or not ***')
+        // check user exist on db or not 
+        const existingUser = await prisma.user.findUnique({
+            where: {
+                email: adminData.email
+            }
+        })
+
+        if(existingUser){
+            throw new Error('User already exists!!')
+        }
+
+        const signUpAdmin = await fetch("http://localhost:3000/api/auth/sign-up/email",{
+            method:'POST',
+            headers:{
+                'content-type':'application/json'
+            },
+            body: JSON.stringify(adminData)
+        })
+
+        if(signUpAdmin.ok){
+            console.log('*** Admin Created ***')
+            await prisma.user.update({
+                where: {
+                    email: adminData.email
+                },
+                data: {
+                    emailVerified:true
+                }
+            })
+            console.log('*** Email verification status updated! ***')
+        }
+        console.log('*** SUCCESS ****')
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+seedAdmin()
